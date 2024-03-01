a6eb7d190044b254dc98c0447897b5556592787e
MadhuraRadkar <142924000+MadhuraRadkar@users.noreply.github.com>
2024-03-01 10:22:00 +0530

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,128 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ReactiveForm2';
+export class AppComponent  implements OnInit{
+  // title = 'ReactiveFormDemo';
+   constructor(private fb:FormBuilder){}
+ 
+ 
+ registrationForm:any;
+ ngOnInit(){
+   this.registrationForm=this.fb.group({
+     userName:['',[Validators.required,Validators.minLength(2)]],
+     Salary:['',[Validators.required]],
+     Department:['',[Validators.required]],
+     password:['',Validators.required],
+     confirmPassword:[''],
+     email:[''],
+     subscribe:[false],
+     address:this.fb.group({
+       city:[''],
+       state:[''],
+       postalCode:['']
+     }),
+     alternateEmail:this.fb.array([])
+     //,{validator:PasswordValidator}
+   });
+ 
+ 
+   // conditionally applied validation on email
+   this.registrationForm.get('subscribe').valueChanges.subscribe((checkedValue:any)=>{
+     const email=this.registrationForm.get('email');
+     if(checkedValue){
+       email.setValidators(Validators.required)
+     }
+     else
+     {
+       email.clearValidators();
+     }
+     // to reflect changes in model
+     email.updateValueAndValidity();
+   })
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ get userName()
+ {
+   return this.registrationForm.get('userName');
+ }
+ get Salary(){
+  return this.registrationForm.get('Salary');
+ }
+ get Department(){
+  return this.registrationForm.get('Department');
+ }
+ 
+ get email()
+ {
+   return this.registrationForm.get('email');
+ }
+ get password()
+ {
+   return this.registrationForm.get('password');
+ }
+ // code to adding controls dynamically
+ get alternateEmail()
+ {
+   return this.registrationForm.get('alternateEmail') as FormArray;
+ }
+ 
+ 
+ addAlternateEmail()
+ {
+   this.alternateEmail.push(this.fb.control(['']));
+ }
+ // using FormGroup and FormControl Class
+   // registrationForm=new FormGroup({
+   //   userName:new FormControl(''),
+   //   password:new FormControl(''),
+   //   confirmPassword:new FormControl(''),
+   //   address:new FormGroup({
+   //     city:new FormControl(''),
+   //     state:new FormControl(''),
+   //     postalCode:new FormControl(''),
+   //   })
+   // });
+ 
+ 
+   loadData()
+   {
+     // setValue needs all values of form control
+     // this.registrationForm.setValue({
+     //   userName:"Aniket",
+     //   password:'123',
+     //   confirmPassword:'123',
+     //   email:'',
+     //   subscribe:false,
+     //   alternateEmail:'',
+     //   address:{
+     //     city:'Aurangabad',
+     //     state:'MH',
+     //     postalCode:123456
+     //   }
+     // });
+     // This can update partial update in form control
+         this.registrationForm.patchValue({
+       userName:"Aniket",
+       password:'123',
+       confirmPassword:'123',
+     });
+   }
+ 
+ 
+   // submit form data
+   onSubmit()
+   {
+     console.log(this.registrationForm.value);
+   }
+ 
 }
